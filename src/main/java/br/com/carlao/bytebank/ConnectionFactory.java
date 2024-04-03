46079581e9afb0384c492c18b3e2c03ef9746ea9
CarlosBotelho42 <carlospiece55@gmail.com>
2024-04-02 23:46:13 -0300

    feat: criação da classe ConnectionFactory para que ela seja reutilizada sempre que precisar de uma conexao com o banco

---
+++ src/main/java/br/com/carlao/bytebank/ConnectionFactory.java
@@ -0,0 +1,17 @@
+package br.com.carlao.bytebank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    public Connection conex(){
+        try {
+           return DriverManager.getConnection("jdbc:mysql://localhost/api_mediciner?user=carlao&password=123321");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
